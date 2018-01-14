5d78112669ded26aabe7b3b5bbe3db37ef5cb287
PhilipChakram <philip.chakram@gmail.com>
2018-01-14 18:11:22 -0500

    Finished Auth

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys.js');
+require('./models/user.js');
+require('./services/passport.js');
 
-app.get('/',(req,res)=>{
-  res.send({bye:'buddy'});
-})
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30*24*60*60*1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT)
