00a5a26c5de0242257f47cfea37aba16c413713b
PhilipChakram <philip.chakram@gmail.com>
2018-01-20 19:55:00 -0500

    Users can see survey results

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,11 @@ import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
-import 'materialize-css/dist/css/materialize.min.css'
 
-import registerServiceWorker from './registerServiceWorker';
+import 'materialize-css/dist/css/materialize.min.css'
+//Dev axios helpers
+import axios from 'axios';
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys.js');
-require('./models/user.js');
+require('./models/User.js');
+require('./models/Survey.js');
 require('./services/passport.js');
 
 mongoose.connect(keys.mongoURI);
@@ -25,6 +26,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if(process.env.NODE_ENV === 'production')
 {
